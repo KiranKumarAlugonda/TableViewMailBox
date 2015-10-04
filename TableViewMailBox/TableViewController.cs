b3767ea7c869bd887b8996047b500e2786318050
Kumar, Kiran <KKumar@massmutual.com>
2015-10-04 13:29:04 -0400

    adding table view logic for tableview controller

--- TableViewMailBox/TableViewController.cs
+++ TableViewMailBox/TableViewController.cs
@@ -2,13 +2,30 @@ using Foundation;
 using System;
 using System.CodeDom.Compiler;
 using UIKit;
+using CoreGraphics;
 
 namespace TableViewMailBox
 {
-	partial class TableViewController : UITableViewController
-	{
-		public TableViewController (IntPtr handle) : base (handle)
-		{
-		}
-	}
+    partial class TableViewController : UITableViewController
+    {
+        EmailServer emailServer = new EmailServer();
+
+        public TableViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return emailServer.Email.Count;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            UITableViewCell cell = new UITableViewCell(CGRect.Empty);
+            var item = emailServer.Email[indexPath.Row];
+
+            cell.TextLabel.Text = item.Subject;
+            return cell;
+        }
+    }
 }
