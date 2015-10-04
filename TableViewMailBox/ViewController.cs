6e8be89c70f26b0b591cd9a50933b1ae587e43e4
Kumar, Kiran <KKumar@massmutual.com>
2015-10-04 12:59:18 -0400

    Basic Table View

---
+++ TableViewMailBox/ViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using UIKit;
+
+namespace TableViewMailBox
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle)
+            : base(handle)
+        {
+        }
+        UITableView tableView;
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            tableView = new UITableView(this.View.Frame);
+            this.View.Add(tableView);
+
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
