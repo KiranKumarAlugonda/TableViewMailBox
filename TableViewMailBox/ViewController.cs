06da63b992749f651a569c06895d05dfc1a80b6e
Kumar, Kiran <KKumar@massmutual.com>
2015-10-04 14:15:27 -0400

    Adding images, label, sub label and table styles

--- TableViewMailBox/ViewController.cs
+++ TableViewMailBox/ViewController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 
 using UIKit;
+using Foundation;
+using CoreGraphics;
 
 namespace TableViewMailBox
 {
@@ -11,11 +13,40 @@ namespace TableViewMailBox
         {
         }
         UITableView tableView;
+        class EmailServerDataSource : UITableViewDataSource
+        {
+            EmailServer emailServer = new EmailServer();
+
+            public override nint RowsInSection(UITableView tableview, nint section)
+            {
+                return emailServer.Email.Count;
+            }
+
+            public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+            {
+                UITableViewCell cell = new UITableViewCell(UITableViewCellStyle.Value1,null);
+                var item = emailServer.Email[indexPath.Row];
+
+                //cell.TextLabel.Text = item.Subject;
+                cell.TextLabel.Text = item.Subject.Substring(0,20)+ "...";
+                cell.DetailTextLabel.Text = item.Body;
+                if(cell.ImageView != null)
+                   cell.ImageView.Image = item.GetImage();
+                cell.TextLabel.TextColor = UIColor.Blue;
+                cell.DetailTextLabel.TextColor = UIColor.Gray;
+                cell.Accessory = UITableViewCellAccessory.Checkmark;
+                return cell;
+            }
+        }
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
+
             tableView = new UITableView(this.View.Frame);
+
+            tableView.DataSource = new EmailServerDataSource();
+
             this.View.Add(tableView);
 
         }
